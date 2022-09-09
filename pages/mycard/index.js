10d088568e82c0d2aa5da1fce4f5e5f879caa7ac
josion1020 <353750904@qq.com>
2022-09-09 17:07:39 +0800

    添加更新检测功能；展示图片添加时间戳

--- pages/mycard/index.js
+++ pages/mycard/index.js
@@ -1,5 +1,24 @@
 // pages/mycard/index.js
-var app = getApp()
+var app = getApp();
+const updateManager = wx.getUpdateManager()
+
+updateManager.onCheckForUpdate(function (res) {
+  // 请求完新版本信息的回调
+  console.log(res.hasUpdate)
+})
+
+updateManager.onUpdateReady(function () {
+  wx.showModal({
+    title: '更新提示',
+    content: '新版本已经准备好，是否重启应用？',
+    success: function (res) {
+      if (res.confirm) {
+        // 新的版本已经下载好，调用 applyUpdate 应用新版本并重启
+        updateManager.applyUpdate()
+      }
+    }
+  })
+})
 Page({
 
   /**
@@ -17,7 +36,8 @@ Page({
     showEdit: false,
     showPage:false,
     id:null,
-    width:0
+    width:0,
+    timestamp:new Date().getTime()
   },
 
   /**
