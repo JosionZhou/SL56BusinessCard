2b29b452e0d04b4d27d813d5f6c78420368f53da
josion1020 <353750904@qq.com>
2022-09-12 14:29:14 +0800

    修改成立年限标签；修改检查更新代码

--- pages/mycard/index.js
+++ pages/mycard/index.js
@@ -1,24 +1,6 @@
 // pages/mycard/index.js
 var app = getApp();
-const updateManager = wx.getUpdateManager()
-
-updateManager.onCheckForUpdate(function (res) {
-  // 请求完新版本信息的回调
-  console.log(res.hasUpdate)
-})
-
-updateManager.onUpdateReady(function () {
-  wx.showModal({
-    title: '更新提示',
-    content: '新版本已经准备好，是否重启应用？',
-    success: function (res) {
-      if (res.confirm) {
-        // 新的版本已经下载好，调用 applyUpdate 应用新版本并重启
-        updateManager.applyUpdate()
-      }
-    }
-  })
-})
+const updateManager = wx.getUpdateManager();
 Page({
 
   /**
@@ -38,7 +20,7 @@ Page({
     id:null,
     width:0,
     timestamp:new Date().getTime(),
-    establishYears:new Date().getFullYear()-2012
+    establishYears:new Date().getFullYear()-2002
   },
 
   /**
@@ -120,6 +102,22 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
+  updateManager.onCheckForUpdate(function (res) {
+    // 请求完新版本信息的回调
+    console.log(res.hasUpdate)
+  });
+  updateManager.onUpdateReady(function () {
+    wx.showModal({
+      title: '更新提示',
+      content: '新版本已经准备好，是否重启应用？',
+      success: function (res) {
+        if (res.confirm) {
+          // 新的版本已经下载好，调用 applyUpdate 应用新版本并重启
+          updateManager.applyUpdate()
+        }
+      }
+    })
+  });
     this.setData({
       showEdit: app.globalData.showEdit
     });
